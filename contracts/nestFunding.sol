bb85af961597041db1bdf5e2c48ea1cf298b379f
Sayantanmpaul <sayantan.college6291@gmail.com>
2023-11-12 01:47:02 +0530

    update campeign util base function

--- contracts/nestFunding.sol
+++ contracts/nestFunding.sol
@@ -19,16 +19,56 @@ contract NestCrowdFunding {
     //keeping track num of contributors
     uint256 public numOfCampagins=0;
 
-    function createCampagin(){
+    //campagin details which is visible to the public
+    function createCampagin(address _campaginOwner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _images) public returns (uint256) {
+
+        Campagin storage campagin= campagins[numOfCampagins];
+        //deadline checkpoint
+        require(campagin.deadlineProject< block.timestamp, "the deadline should be a target date for future of minimum 12 hours! ");
+
+        campagin.userOwner= _campaginOwner;
+        campagin.title= _title;
+        campagin.description= _description;
+        campagin.targetAmmount= _target;
+        campagin.deadlineProject= _deadline;
+        campagin.amaountAchived= 0;
+        campagin.images= _images;
+        
+        numOfCampagins++;
+
+        return numOfCampagins-1;
+    }
+
+    //contribute campaign function
+    function contributeCampagin(uint256 _ID) public payable{
+        uint256 amaountValue= msg.value;
+        
+        Campagin storage campaign= campagins[_ID];
+        campaign.contributors.push(msg.sender);
+        campaign.donations.push(amaountValue);
+
+        (bool sent, )= payable(campaign.userOwner).call{value: amaountValue}("");
+        if(sent){
+            campaign.amaountAchived= campaign.amaountAchived+ amaountValue;
+        }
 
     }
-    function contributeCampagin(){
 
+    //contribution donators transactions
+    function getContributors(uint256 _userID)public view returns(address[] memory, uint256[] memory){
+        return(campagins[_userID].contributors, campagins[_userID].donations);
     }
-    function getContributors(){
 
-    }
-    function listGetCampagins(){
+    //campeign list update
+    function listGetCampagins() public view returns(Campagin[] memory){
+        Campagin[] memory allCampaigns= new Campagin[](numOfCampagins);
 
+        for(uint i=0; i<numOfCampagins; i++){
+            Campagin storage item= campagins[i];
+
+            allCampaigns[i]=item;
+        }
+
+        return allCampaigns;
     }
 }

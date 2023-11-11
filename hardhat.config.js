0036077e60da2f6aa5c73e8a1a3f92eafa262d93
Sayantanmpaul <sayantan.college6291@gmail.com>
2023-11-12 01:47:51 +0530

    update hardhat.config.js with goerli deployment added

--- hardhat.config.js
+++ hardhat.config.js
@@ -33,6 +33,15 @@ module.exports = {
   },
   solidity: {
     version: "0.8.17",
+    //eth network and url added
+    defaultNetwork:'goerli',
+    networks: {
+      hardhat:{},
+      goerli:{
+        url: 'https://rpc.ankr.com/eth_goerli',
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
+      }
+    },
     settings: {
       optimizer: {
         enabled: true,
